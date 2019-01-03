47ae42c2d50af879ad2ed69deaf8c4e81f99059a
Marcuso83 <46351954+Marcuso83@users.noreply.github.com>
2019-01-03 16:55:09 +0100

    Add files via upload

---
+++ 20190103_Capstone_LAB.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is related to a Capstone project.
+
+# In[1]:
+
+
+#import relevant libraries
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+#print statement
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
